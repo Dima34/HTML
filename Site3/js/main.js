57d58369aa5292fbbd21cc5a778a7cc91c34c5a2
Dima34 <57358582+Dima34@users.noreply.github.com>
2020-09-19 00:49:53 +0300

    Anim

--- Site3/js/main.js
+++ Site3/js/main.js
@@ -118,71 +118,144 @@ window.onload = function(){
         }
     });
     
-} ()
 
-// Animation
-document.addEventListener("DOMContentLoaded", scrolling);
-document.addEventListener("scroll",scrolling);
+    // Animation
+    document.addEventListener("DOMContentLoaded", scrolling);
+    document.addEventListener("scroll",scrolling);
 
-function isPartiallyVisible(el){
-    var elementBoundary = el.getBoundingClientRect();
-    var top = elementBoundary.top - 71;
-    var bottom = elementBoundary.bottom;
-    var height = elementBoundary.height;
+    function isPartiallyVisible(el){
+        var elementBoundary = el.getBoundingClientRect();
+        var top = elementBoundary.top - 71;
+        var bottom = elementBoundary.bottom;
+        var height = elementBoundary.height;
 
-    return((top + height >= 0) && (height + window.innerHeight >= bottom));
-}
-function isFullyVisible(el) {
-    var elementBoundary = el.getBoundingClientRect();
-    
-    var top = elementBoundary.top;
-    var bottom = elementBoundary.bottom;
-
-    return ((top >= 0) && (bottom <= window.innerHeight));
-}
-
-// Elements
-function animPartiallyVisible(elem,Class){
-    if(isPartiallyVisible(elem)){
-        elem.classList.add(Class);
-    } else{
-        elem.classList.remove(Class);
+        return((top + height >= 0) && (height + window.innerHeight >= bottom));
     }
-}
-function animPartiallyVisibleALL(elem,Class,arrLen){
-    for(var i = 0; i < arrLen;i++){
-        if(isPartiallyVisible(elem[i])){
-            elem[i].classList.add(Class);
+    function isFullyVisible(el) {
+        var elementBoundary = el.getBoundingClientRect();
+        
+        var top = elementBoundary.top;
+        var bottom = elementBoundary.bottom;
+
+        return ((top >= 0) && (bottom <= window.innerHeight));
+    }
+
+    // Elements
+    function animPartiallyVisible(elem,Class){
+        if(isPartiallyVisible(elem)){
+            elem.classList.add(Class);
         } else{
-            elem[i].classList.remove(Class);
+            elem.classList.remove(Class);
         }
     }
+    function animPartiallyVisibleALL(elem,Class){
+        for(var i = 0; i < elem.length;i++){
+            if(isPartiallyVisible(elem[i])){
+                elem[i].classList.add(Class);
+            } else{
+                elem[i].classList.remove(Class);
+            }
+        }
+        
+    }
+    function animPartiallyVisibleRaALL(elem,toElem,Class,arrLen){
+        for(var i = 0; i < arrLen;i++){
+            if(isPartiallyVisible(elem[i])){
+                toElem[i].classList.add(Class);
+            } else{
+                toElem[i].classList.remove(Class);
+            }
+        }
+        
+    }
+    function animPartiallyVisibleRaChildALL(elem,toElem,Class){
+        for(var i = 0; i < toElem.length;i++){
+            if(isPartiallyVisible(elem)){
+                toElem[i].classList.add(Class);
+            } else{
+                toElem[i].classList.remove(Class);
+            }
+        }
+        
+    }
+    function animPartiallyVisibleRaChild(elem,toElem,Class){
+            if(isPartiallyVisible(elem)){
+                toElem.classList.add(Class);
+            } else{
+                toElem.classList.remove(Class);
+            }
+        
+        
+    }
+
+    let introTitle = document.querySelector(".intro__title")
+    let introSubtitle = document.querySelector(".intro__subtitle");
+    let introSearchForm = document.querySelector(".search-form__intro");
+    let benefitsTitle = document.querySelector(".benefits__title");
+    let benefitsTitle_span = document.querySelectorAll(".benefits__title span")
+    let benefitsCardThumb = document.querySelectorAll(".benefits__card-thumb")
+    let benefitsCardText = document.querySelectorAll(".benefits__card-title, .benefits__card-desc, .benefits__card-more")
+    let placesTitle = document.querySelector(".places__title")
+    let sectionTitle = document.querySelectorAll(".section-title")
+    let placesTitle_span = document.querySelectorAll(".places__title span")
+    let placesCard = document.querySelectorAll(".places__card")
+    let placesCard_wrap = document.querySelectorAll(".places__card-wrap")
+    let toursTour = document.querySelectorAll(".tour__pic, .tour__info")
+    let toursTour_leftSide = [toursTour[0], toursTour[2]];
+    let toursTour_rightSide = [toursTour[1], toursTour[3]];
+    let topTitle = document.querySelector(".top-title")
+    let topTitle_span = document.querySelectorAll(".top-title span")
+    let topCard = document.querySelectorAll(".top__card")
+    let videos = document.querySelector(".videos")
+    let videosWrapper = document.querySelector(".videos__wrapper")
+
+
+
+    // Movement
+    function scrolling(){
+        animPartiallyVisible(introTitle,"intro__title-anim")
+        animPartiallyVisible(introSubtitle,"intro__subtitle-anim")
+        animPartiallyVisible(introSearchForm,"serch-form-anim")
+        animPartiallyVisibleRaChildALL(benefitsTitle,benefitsTitle_span, "benefits__title-anim", benefitsTitle_span.length)
+        animPartiallyVisibleALL(benefitsCardThumb, "benefits__card-thumb-anim")
+        animPartiallyVisibleALL(benefitsCardText, "benefits__card-text-anim")
+        animPartiallyVisibleRaChildALL(placesTitle,placesTitle_span, "section-title-anim")
+        animPartiallyVisible(sectionTitle[0], "section-title-after-anim")
+        animPartiallyVisibleRaALL(placesCard,placesCard_wrap, "places__card-anim", placesCard.length)
+        animPartiallyVisibleALL(toursTour_leftSide,"tours__tour_leftSide-anim")
+        animPartiallyVisibleALL(toursTour_rightSide,"tours__tour_rightSide-anim")
+        animPartiallyVisibleRaChildALL(topTitle,topTitle_span, "section-title-anim")
+        animPartiallyVisible(sectionTitle[1], "top-title-after-anim")
+        animPartiallyVisibleRaChild(videos, videosWrapper, "videos__wrapper-anim")
+        
+        if(window.innerWidth > 1015){
+            for(let i = 0; i < topCard.length; i+=3){ //left side
+                animPartiallyVisible(topCard[i],"top__card-left-side-anim")
+            }
+            for(let i = 1; i < topCard.length; i+=3){ //center
+                let cardInner = topCard[i].querySelectorAll(".top__card-pic, .top__card-desc, .top__card-more")
+                animPartiallyVisibleRaChildALL(topCard[i],cardInner,"top__card-center-anim")
+            }
+            for(let i = 2; i < topCard.length; i+=3){ // right side
+                animPartiallyVisible(topCard[i],"top__card-right-side-anim")
+            }
+        }
+        else {
+            for(let i = 0; i < topCard.length; i++){
+                let cardInner = topCard[i].querySelectorAll(".top__card-pic, .top__card-desc, .top__card-more")
+                animPartiallyVisible(cardInner[0],"top__card-all-anim")
+                animPartiallyVisible(cardInner[1],"top__card-all-anim")
+                animPartiallyVisible(cardInner[2],"top__card-all-anim")
+            }
+        }
+
+
+        
+    }
     
-}
-
-var introTitle = document.querySelector(".intro__title")
-var introSubtitle = document.querySelector(".intro__subtitle");
-var introSearchForm = document.querySelector(".search-form__intro");
-var benefitsTitle = document.querySelectorAll(".benefits__title span")
-var benefitsCardThumb = document.querySelectorAll(".benefits__card-thumb")
-var benefitsCardText = document.querySelectorAll(".benefits__card-title, .benefits__card-desc, .benefits__card-more")
-var sectionTitle = document.querySelectorAll(".section-title")
-var sectionTitleAfter = document.querySelectorAll(".section-title:after")
-var placesCard = document.querySelectorAll(".places__card")
+} ()
 
 
-// Movement
-function scrolling(){
-    animPartiallyVisible(introTitle,"intro__title-anim")
-    animPartiallyVisible(introSubtitle,"intro__subtitle-anim")
-    animPartiallyVisible(introSearchForm,"serch-form-anim")
-    animPartiallyVisibleALL(benefitsTitle, "benefits__title-anim",benefitsTitle.length)
-    animPartiallyVisibleALL(benefitsCardThumb, "benefits__card-thumb-anim", benefitsCardThumb.length)
-    animPartiallyVisibleALL(benefitsCardText, "benefits__card-text-anim", benefitsCardText.length)
-    animPartiallyVisibleALL(sectionTitle, "section-title-anim", sectionTitle.length)
-    animPartiallyVisibleALL(sectionTitleAfter, ".section-title-anim:after", sectionTitleAfter.length)
-    animPartiallyVisibleALL(placesCard, "places__card-anim", placesCard.length)
-}
     
 
 
