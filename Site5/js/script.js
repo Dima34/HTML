142e722b61ffab4cfd686569e0d95a7c3783a78d
Dima34 <57358582+Dima34@users.noreply.github.com>
2020-10-03 15:19:21 +0300

    js added

--- Site5/js/script.js
+++ Site5/js/script.js
@@ -1,6 +1,54 @@
-// (function(){
-    // let serv_price_blocks = document.querySelectorAll(".serv-price-block")
-    // console.log( typeof (serv_price_blocks))
-    // serv_price_blocks = Object.enteries(serv_price_blocks)
-    // console.log( typeof (serv_price_blocks))
-// }())
+(function(){
+    let serv_price_blocks = document.querySelectorAll(".serv-price-block")
+    let qa_blocks = document.querySelectorAll(".qa-block")
+
+    for(let i = 0; i < serv_price_blocks.length; i++){
+        serv_price_blocks[i].addEventListener("click",() => {
+            classlist = Object.values(serv_price_blocks[i].classList)
+            console.log()
+            if(classlist.indexOf("serv-price-block-active") != -1){
+                serv_price_blocks[i].classList.remove("serv-price-block-active");
+            }
+            else{
+                serv_price_blocks[i].classList.add("serv-price-block-active");
+            }
+        })
+    }
+    for(let i = 0; i < qa_blocks.length; i++){
+        qa_blocks[i].addEventListener("click",() => {
+            classlist = Object.values(qa_blocks[i].classList)
+            console.log()
+            if(classlist.indexOf("qa-block-active") != -1){
+                qa_blocks[i].classList.remove("qa-block-active");
+            }
+            else{
+                qa_blocks[i].classList.add("qa-block-active");
+            }
+        })
+    }
+
+    let info__nav_items = document.querySelectorAll(".info__nav_item-link");
+    let nav_blocks = document.querySelectorAll(".nav__blocks_block");
+    let info__nav_items_list = Object.values(info__nav_items);
+    let nav_blocks_list = Object.values(nav_blocks);
+    console.log(info__nav_items_list)
+    let defaultItemActive = 0;
+
+    info__nav_items_list[defaultItemActive].classList.add("info__nav_item-link-active")
+    nav_blocks_list[defaultItemActive].classList.add("block-active")
+    for(let i = 0; i < info__nav_items_list.length;i++){
+        info__nav_items_list[i].addEventListener("click", () => {
+            for(let b = 0; b < info__nav_items_list.length; b++){
+                if(i != b){
+                    info__nav_items_list[b].classList.remove("info__nav_item-link-active")
+                    nav_blocks_list[b].classList.remove("block-active")
+                }
+                else{
+                    info__nav_items_list[b].classList.add("info__nav_item-link-active")
+                    nav_blocks_list[b].classList.add("block-active")
+                }
+            }
+        })
+    }
+
+}())
