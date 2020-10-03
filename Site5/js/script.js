084b80f19278a11a3c198b9a36550bbfc2bb396e
Dima34 <57358582+Dima34@users.noreply.github.com>
2020-10-03 12:14:25 +0300

    site 5 update

--- Site5/js/script.js
+++ Site5/js/script.js
@@ -1,22 +1,6 @@
 (function(){
-    burger = document.querySelector(".header__nav-burger")
-    navItem = document.querySelectorAll(".header__nav-item")
-    burger.addEventListener("click", () => {
-        let class_arr = Object.values(burger.classList)
-        if(class_arr.indexOf("header__nav-burger-active") != -1){
-            burger.classList.remove("header__nav-burger-active")
-            for(let i = 0; i < navItem.length; i++){
-                navItem[i].classList.remove("header__nav-item-active")
-            }
-            
-        }
-        else{
-            burger.classList.add("header__nav-burger-active")
-            for(let i = 0; i < navItem.length; i++){
-                navItem[i].classList.add("header__nav-item-active")
-            }
-        }
-        
-        
-    })
+    let serv_price_blocks = document.querySelectorAll(".serv-price-block")
+    console.log( typeof (serv_price_blocks))
+    serv_price_blocks = Object.enteries(serv_price_blocks)
+    console.log( typeof (serv_price_blocks))
 }())
