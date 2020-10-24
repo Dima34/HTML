5ae27d62d62b5d3fd3b4cf84db314e14a59ae40a
Your Name <you@example.com>
2020-10-24 04:03:32 +0300

    memu is done

--- Site7/js/script.js
+++ Site7/js/script.js
@@ -4,27 +4,60 @@
     let burger = document.querySelector(".header-burger");
     let main_menu = document.querySelector(".header-menu");
     let main_menu_close = document.querySelector(".header_menu-close-btn");
+    let main_menu_subpages = document.querySelectorAll(".items_sub-menu");
+    let main_menu_subpages_links = document.querySelectorAll(".main-menu_first-list .main-menu_list-item");
+    let main_menu_subpages_btn_back = document.querySelectorAll(".sub-menu_header-link");
 
     burger.addEventListener("click", () => {
         main_menu.classList.add("x_active")
     })
     main_menu_close.addEventListener("click", () => {
-        main_menu.classList.remove("x_active")
+        main_menu.classList.remove("x_active");
+        for(let b = 0; b < main_menu_subpages_links.length; b++){
+            main_menu_subpages[b].classList.remove("x_active");
+        }
     })
+
+    for(let i = 0; i < main_menu_subpages_btn_back.length; i++){
+        main_menu_subpages_btn_back[i].addEventListener("click",() => {
+            for(let b = 0; b < main_menu_subpages_links.length; b++){
+                main_menu_subpages[b].classList.remove("x_active");
+            }
+        })
+    }
+
+    for(let i = 0; i < main_menu_subpages_links.length; i++){
+        main_menu_subpages_links[i].addEventListener("click",() => {
+            classlist = Object.values(main_menu_subpages[i].classList)
+            main_menu_subpages[i].classList.add("x_active");
+            for(let b = 0; b < main_menu_subpages_links.length; b++){
+                if(b != i){
+                    main_menu_subpages[b].classList.remove("x_active");
+                } 
+            }
+        })
+    }
 
     // Main slider menu
     let slider_items = document.querySelectorAll(".main-menu_nav-item");
     let slider_blocks = document.querySelectorAll(".nav-block");
-
+    let default_slide = 0;
+  
+    slider_blocks[default_slide].classList.add("x_active");
+    slider_items[default_slide].classList.add("nav-item-active");
+    slider_blocks[default_slide].classList.add("size_fix");
+  
     for(let i = 0; i < slider_items.length; i++){
         slider_items[i].addEventListener("click",() => {
             classlist = Object.values(slider_blocks[i].classList)
             slider_blocks[i].classList.add("x_active");
             slider_items[i].classList.add("nav-item-active");
+            slider_blocks[i].classList.add("size_fix");
             for(let b = 0; b < slider_items.length; b++){
                 if(b != i){
                     slider_blocks[b].classList.remove("x_active");
                     slider_items[b].classList.remove("nav-item-active");
+                    slider_blocks[b].classList.remove("size_fix");
                 } 
             }
         })
