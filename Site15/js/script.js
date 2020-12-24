47c67dedd73d71e2535f270295da1d96ce036238
Your Name <you@example.com>
2020-12-25 00:58:49 +0200

    upd

--- Site15/js/script.js
+++ Site15/js/script.js
@@ -16,41 +16,56 @@ ibg();
 
 hasChild = document.querySelectorAll(".has_child");
 
-console.log(hasChild);
-for (let i = 0; i < hasChild.length; i++) {
+function setSubMenuPos(child){
+    let hasChildToBottom = window.innerHeight - (child.getBoundingClientRect().height + child.getBoundingClientRect().y);
+    childList  = child.querySelector(".listitem_child");
+    childHeight = childList.getBoundingClientRect().height;
 
-    hasChild[i].addEventListener("mouseover", ()=>{
-        let hasChildToBottom = window.innerHeight - (hasChild[i].getBoundingClientRect().height + hasChild[i].getBoundingClientRect().y);
-        childList  = hasChild[i].querySelector(".listitem_child");
-        childHeight = childList.getBoundingClientRect().height;
+    if(hasChildToBottom <= (childHeight - child.getBoundingClientRect().height)){
 
-        if(hasChildToBottom <= (childHeight - hasChild[i].getBoundingClientRect().height)){
+        childList.style.top = "unset";
+        childList.style.bottom = "0";
 
-            childList.style.top = "unset";
-            childList.style.bottom = "0";
-
-            if(window.innerHeight <= childHeight){
-                childList.style.overflowY = "scroll";
-                childList.style.maxHeight = window.innerHeight + "px";
-            }
-            else{
-                childList.style.overflowY = "unset";
-                childList.style.maxHeight = "unset";
-            }
+        if(window.innerHeight <= childHeight){
+            childList.style.overflowY = "scroll";
+            childList.style.maxHeight = window.innerHeight + "px";
         }
         else{
-            childList.style.top = hasChild[i].getBoundingClientRect().y + "px";
-            childList.style.bottom = "unset";
-            if(window.innerHeight >= childHeight){
-                console.log('unset');
-                childList.style.overflowY = "unset";
-                childList.style.maxHeight = "unset";
-            }
+            childList.style.overflowY = "unset";
+            childList.style.maxHeight = "unset";
         }
-
-        
-    
-    })
-
-
+    }
+    else{
+        console.log(child.getBoundingClientRect())
+        childList.style.top = child.getBoundingClientRect().top + pageYOffset + "px";
+        childList.style.bottom = "unset";
+        if(window.innerHeight >= childHeight){
+            childList.style.overflowY = "unset";
+            childList.style.maxHeight = "unset";
+        }
+    } 
 }
+
+for (let i = 0; i < hasChild.length; i++) {
+
+    setSubMenuPos(hasChild[i]);
+
+    hasChild[i].addEventListener("mouseover", ()=>{
+        setSubMenuPos(hasChild[i]);
+    })
+}
+
+//
+//  Text copy
+//
+
+let textCopyBlk = document.querySelector(".header_mail");
+//let textToCopy = textCopyBlk.querySelector("a").innerHTML;
+let textCopyBtn = textCopyBlk.querySelector("span");
+
+textCopyBtn.addEventListener('click',()=>{
+    let textToCopy = textCopyBlk.querySelector("a");
+    textToCopy.select
+    document.execCommand("copy");
+    alert("Copied the text: " + textToCopy.value);
+});
