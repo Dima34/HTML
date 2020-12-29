6185f06a4dc15968e9eb9e99accf2987d7c2d4c2
Your Name <you@example.com>
2020-12-29 17:26:35 +0200

    upd

--- Site16/js/script.js
+++ Site16/js/script.js
@@ -240,5 +240,26 @@ if(preOrderOpen){
     });
 }
 
+//
+//  reg switch
+//
 
+let urBtn = document.querySelector(".choise-ur")
+let urBlk = document.querySelector(".main-ur")
 
+let fizBtn = document.querySelector(".choise-fiz")
+let fizBlk = document.querySelector(".main-fiz")
+
+fizBtn.addEventListener('click',()=>{
+    urBlk.style.display = "none"
+    urBtn.classList.remove("reg-choice-selected")
+    fizBlk.style.display = "block"
+    fizBtn.classList.add("reg-choice-selected")
+});
+
+urBtn.addEventListener('click',()=>{
+    fizBlk.style.display = "none"
+    fizBtn.classList.remove("reg-choice-selected")
+    urBlk.style.display = "block"
+    urBtn.classList.add("reg-choice-selected")
+});
