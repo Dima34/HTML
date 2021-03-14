58d2f36a4bb12113a7f50b905dcd320cd1da755c
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-03-14 19:24:15 +0300

    upd

--- Site19/js/script.js
+++ Site19/js/script.js
@@ -13,7 +13,7 @@ ibg();
 
 
 // 
-// Burger open
+// Burger
 // 
 
 let OpenBtn = document.querySelector(".header-burger");
@@ -22,17 +22,74 @@ if(OpenBtn){
 
     let menu = document.querySelector(".header-burger-menu");
     let CloseBtn = document.querySelector(".burger-close");
+    let burgerItems = document.querySelectorAll(".burger_item");
 
     OpenBtn.addEventListener('click',()=>{
         menu.classList.add("active-x")
-        document.getElementById("closeSVG").style.display = "none"
-        document.getElementById("openSVG").style.display = "block"
+        document.getElementById("closeSVG").style.display = "none";
+        document.getElementById("openSVG").style.display = "block";
+
+        let startS = 0.5;
+        for (let i = 0; i < burgerItems.length; i++) {
+            startS +=0.15;
+            burgerItems[i].style.transitionDuration = startS + "s";
+            burgerItems[i].style.transform = "translateX(0px)";
+            opacityCounter(burgerItems[i],25);
+        }
     });
 
     CloseBtn.addEventListener('click',()=>{
         menu.classList.remove("active-x")
-        document.getElementById("closeSVG").style.display = "block"
-        document.getElementById("openSVG").style.display = "none"
-    });
+        document.getElementById("closeSVG").style.display = "block";
+        document.getElementById("openSVG").style.display = "none";
 
+        for (let i = 0; i < burgerItems.length; i++) {
+            opacityReversedCounter(burgerItems[i],5);
+            setTimeout(() => {
+                burgerItems[i].style.transform = "translateX(200%)";
+            }, 500);
+        }        
+    });
 }
+
+opacityCounter = (element,speed) =>{
+    let tmp = 0;
+    let timer = setInterval(()=>{
+        tmp+=0.1;
+        element.style.opacity = tmp;
+        console.log(tmp);
+        if(tmp >=1){
+            clearTimeout(timer)
+        }
+    },speed)
+}
+
+opacityReversedCounter = (element,speed) =>{
+    let tmp = 1;
+    let timer = setInterval(()=>{
+        tmp-=0.1;
+        element.style.opacity = tmp;
+        console.log(tmp);
+        if(tmp <=0){
+            clearTimeout(timer)
+        }
+    },speed)
+}
+
+
+// 
+// Cursor
+// 
+
+let cursorBig = document.querySelector(".cursor-big");
+let cursorSmall = document.querySelector(".cursor-small");
+
+
+
+window.requestAnimationFrame(()=>{
+    window.addEventListener("mousemove", (e)=>{
+        cursorBig.style.left = e.clientX+"px";
+        cursorBig.style.top = e.clientY+"px";
+    });
+    
+})
