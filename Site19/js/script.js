366429f170e7081c5d8c2255b4f410dcbaa170c8
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-03-22 11:03:39 +0300

    upd

--- Site19/js/script.js
+++ Site19/js/script.js
@@ -170,6 +170,10 @@ mouseParallax(document.querySelector(".goal-ring.ring2"), 0.007);
 mouseParallax(document.querySelector(".goal-ring.ring3"), 0.007);
 mouseParallax(document.querySelector(".goal-ring.ring4"), 0.007);
 
+mouseParallax(document.querySelector(".who-is-who .ring1"), 0.002);
+mouseParallax(document.querySelector(".who-is-who .ring2"), 0.002);
+mouseParallax(document.querySelector(".our-pets .ring3"), 0.003);
+mouseParallax(document.querySelector(".our-pets .ring4"), 0.002), true;
 
 
 // 
