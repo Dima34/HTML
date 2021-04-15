0cba4fc233923014b1c5f66200a8a48c6a1bb7bb
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-04-16 01:40:32 +0300

    upd

--- Site20/js/script.js
+++ Site20/js/script.js
@@ -79,8 +79,7 @@ document.addEventListener("click", (e)=>{
   obj = e.path
   
   if(!CheckExistClass(obj, "subscribed")){
-      console.log("close");
-      UnsubscibeDropDown.classList.remove("subscribed-menu-active");
+    UnsubscibeDropDown.classList.remove("subscribed-menu-active");
   }
   
 })
@@ -117,10 +116,8 @@ document.addEventListener("click", (e)=>{
   obj = e.path
 
   if(!CheckExistClass(obj, "write-new")){
-    console.log("close");
     WriteNewBlk.classList.remove("input-active");
   }
-  
 })
 
 // 
@@ -135,12 +132,8 @@ if(moreBlk.length >0){
     
     moreBlk[i].addEventListener("click", ()=>{
       moreBlk[i].classList.add("more-option-active")
-    })
-    
+    }) 
   }
-  
-
-
 }
 
 document.addEventListener("click", (e)=>{
@@ -197,8 +190,253 @@ if(raitingBlk.length > 0){
         raitingDown.classList.remove("active");
       }
     })
-
   }
+}
+
+// 
+// Comment system
+// 
+
+updateComments()
+
+function updateComments(){
+  let postComment = document.querySelectorAll(".post_comments");
+
+  if(postComment.length > 0){
+
+    for (let i = 0; i < postComment.length; i++) {
+      console.log("check ", i);
+      
+      let mainCommentInput = postComment[i].querySelector(".main-comment-input > .comment-input");
+
+      mainCommentInput.querySelector(".send-message").addEventListener("click", ()=>{
+        CreateCommentBlock(postComment[i].querySelector(".comments-wrapper"),"./img/profile-photo.png" ,"Dima", "Hello!", "22")
+        updateComments()
+      })
+
+      // Ответ подкомментариев
+      let subCommentInput = postComment[i].querySelector(".sub-comment-blk").querySelector(".comment-input");
+      let subCommentGroup = postComment[i].querySelector(".sub-comment-group");
+      let subCommentAnswerBtn = postComment[i].querySelector(".sub-comment-group").querySelectorAll(".answer");
+      
+      // При клике проявляем инпут и вставляем имя того, кто упомянул
+      subCommentAnswerBtn.forEach((answerBtn)=>{
+        SubCommentInputOpen(answerBtn);
+      }) 
+
+      // При клике на кнопку постится блок с текстом
+      subCommentInput.querySelector(".send-message").addEventListener("click", ()=>{
+
+        let subCommentInputText = subCommentInput.querySelector(".textarea");
+        
+        if(!(subCommentInputText.innerHTML.length - subCommentInputText.querySelector(".mention").textContent.length < 38)){
+          CreateSubCommentBlock(subCommentGroup, "./img/profile-photo.png", "Ivan Ivanov", subCommentInputText.innerHTML, "16.04.2021");
+          subCommentAnswerBtn = UpdateSubAnswerBtns(postComment[i])
+
+          subCommentAnswerBtn.forEach((answerBtn)=>{
+            SubCommentInputOpen(answerBtn);
+          }) 
+        }
+
+        CloseAndCleanAllSubCommmentInputs();
+      })
+
+      // Ответ комментариев
+      let CommentAnswerBtn = postComment[i].querySelector(".main-comment").querySelector(".answer");
+      
+      SubCommentInputOpen(CommentAnswerBtn);
+    }
+  }
+}
+
+// При клике проявляем инпут и вставляем имя того, кто упомянул
+function SubCommentInputOpen(AnswerBtn){
+  AnswerBtn.addEventListener("click", ()=>{
+    let mentorName = AnswerBtn.parentNode.parentNode.querySelector(".name").textContent;
+
+    let subCommentInput = AnswerBtn.parentNode.parentNode.parentNode.parentNode.querySelector(".comment-input");
+    subCommentInput.classList.add("active");
+    subCommentInput.querySelector(".textarea").innerHTML= `<span class="mention">${mentorName},</span> &nbsp; `;
+  })
   
+}
+
+function CloseAndCleanAllSubCommmentInputs(){
+  let SubCommentInput = document.querySelectorAll(".sub-comment-blk .sub-comment-input .comment-input");
+  let SubCommentInputTextArea = document.querySelector(".textarea");
+  SubCommentInputTextArea.innerHTML = `<span class="placeholder">${SubCommentInputTextArea.getAttribute("placeholder")}</span>`
+
+  SubCommentInput.forEach((el)=>{
+    el.classList.remove("active")
+  })
+}
+
+function UpdateSubAnswerBtns(postCommentBlock){
+  subCommentAnswerBtn = postCommentBlock.querySelector(".sub-comment-group").querySelectorAll(".answer");
+  
+  return subCommentAnswerBtn;
+}
+
+function UpdateAnswerBtns(postCommentBlock){
+  CommentAnswerBtn = postCommentBlock.querySelector(".sub-comment-group").querySelectorAll(".answer");
+  
+  return CommentAnswerBtn;
+}
+
+function CreateCommentBlock(whereToAddBlk,imageSource,name, text, date){
+
+  let CommentBlockTemplate = `
+  <div class="comment-blk">
+  <div class="comment main-comment">
+      <div class="comment_header">
+          <img src="${imageSource}" alt="thumb">
+          <p class="name">
+            ${name}
+          </p>
+      </div>
+
+      <div class="comment_text">
+          <p>
+            ${text}
+          </p>
+      </div>
+
+      <div class="comment_footer">
+          <p class="date">
+            ${date}
+          </p>
+          <button class="answer">to answer</button>
+      </div>
+
+      <div class="comment_likes">
+          <p>
+              <span class="likes-amount">0</span>
+              <span>
+                <img src="./img/svg/comment-like.svg" alt="like">
+              </span>
+          </p>
+      </div>
+  </div>
+
+  <div class="sub-comment-blk">
+
+      <div class="sub-comment-gpoup">
+      </div>
+      
+      <div class="sub-comment-input">
+          <div class="comment-input">
+              <img src="./img/profile-photo.png" alt="thumb" class = "user-thumb">
+              <div class="input-group">
+                  <textarea rows='1' placeholder = "Write a comment" id = "text1"></textarea>
+
+                  <div class="button-group">
+                      <button class="send-photo">
+                          <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M21.125 21.125H4.875C4.44402 21.125 4.0307 20.9538 3.72595 20.649C3.4212 20.3443 3.25 19.931 3.25 19.5V8.125C3.25 7.69402 3.4212 7.2807 3.72595 6.97595C4.0307 6.67121 4.44402 6.5 4.875 6.5H8.1244L9.7494 4.0625H16.2494L17.8744 6.5H21.125C21.556 6.5 21.9693 6.67121 22.274 6.97595C22.5788 7.2807 22.75 7.69402 22.75 8.125V19.5C22.75 19.931 22.5788 20.3443 22.274 20.649C21.9693 20.9538 21.556 21.125 21.125 21.125Z" stroke="#3F3F3F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                              <path d="M13 17.0625C15.0193 17.0625 16.6562 15.4255 16.6562 13.4062C16.6562 11.387 15.0193 9.75 13 9.75C10.9807 9.75 9.34375 11.387 9.34375 13.4062C9.34375 15.4255 10.9807 17.0625 13 17.0625Z" stroke="#3F3F3F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                          </svg>    
+                      </button>
+
+                      <button class="send-file">
+                          <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M16.2494 8.12451L7.78845 16.726C7.4837 17.0307 7.3125 17.444 7.3125 17.875C7.3125 18.306 7.4837 18.7193 7.78845 19.0241C8.0932 19.3288 8.50652 19.5 8.9375 19.5C9.36848 19.5 9.7818 19.3288 10.0865 19.0241L20.1725 8.79761C20.4743 8.49582 20.7137 8.13754 20.877 7.74323C21.0403 7.34892 21.1244 6.92631 21.1244 6.49951C21.1244 6.07272 21.0403 5.6501 20.877 5.25579C20.7137 4.86148 20.4743 4.5032 20.1725 4.20141C19.8707 3.89962 19.5124 3.66023 19.1181 3.4969C18.7238 3.33358 18.3012 3.24951 17.8744 3.24951C17.4476 3.24951 17.025 3.33358 16.6307 3.4969C16.2364 3.66023 15.8781 3.89962 15.5763 4.20141L5.49035 14.4279C4.57611 15.3421 4.0625 16.5821 4.0625 17.875C4.0625 19.1679 4.57611 20.4079 5.49035 21.3222C6.40459 22.2364 7.64457 22.75 8.9375 22.75C10.2304 22.75 11.4704 22.2364 12.3846 21.3222L20.7182 12.9995" stroke="#3F3F3F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                          </svg> 
+                      </button>
+
+                      <button class="send-emoji">
+                          <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M17.2224 15.4371C16.7941 16.178 16.1784 16.7931 15.4372 17.2207C14.696 17.6483 13.8554 17.8735 12.9997 17.8735C12.1439 17.8735 11.3033 17.6484 10.5621 17.2207C9.82085 16.7931 9.20518 16.178 8.77686 15.4372" stroke="#3F3F3F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                              <path d="M15.8655 22.3221C13.7872 22.9594 11.5554 22.8831 9.52548 22.1054C7.49554 21.3276 5.78413 19.8932 4.66372 18.0303C3.54331 16.1675 3.0783 13.9833 3.34273 11.8257C3.60716 9.66797 4.58582 7.66077 6.12291 6.1236C7.66 4.58644 9.66716 3.60768 11.8248 3.34315C13.9825 3.07862 16.1667 3.54352 18.0296 4.66385C19.8925 5.78417 21.327 7.49551 22.1048 9.52542C22.8827 11.5553 22.9591 13.7871 22.3218 15.8654L15.8655 22.3221Z" stroke="#3F3F3F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                              <path d="M9.34375 11.9844C9.90466 11.9844 10.3594 11.5297 10.3594 10.9688C10.3594 10.4078 9.90466 9.95312 9.34375 9.95312C8.78284 9.95312 8.32812 10.4078 8.32812 10.9688C8.32812 11.5297 8.78284 11.9844 9.34375 11.9844Z" fill="#3F3F3F"/>
+                              <path d="M16.6562 11.9844C17.2172 11.9844 17.6719 11.5297 17.6719 10.9688C17.6719 10.4078 17.2172 9.95312 16.6562 9.95312C16.0953 9.95312 15.6406 10.4078 15.6406 10.9688C15.6406 11.5297 16.0953 11.9844 16.6562 11.9844Z" fill="#3F3F3F"/>
+                          </svg>   
+                      </button>
+
+                      <button class="send-message">
+                          send
+                      </button>
+                  </div>
+                  
+              </div>
+          </div>
+      </div>
+  </div>
+  </div>
+
+  `
+
+  whereToAddBlk.innerHTML += CommentBlockTemplate;
 
 }
+
+function CreateSubCommentBlock(whereToAddBlk,imageSource,name, text, date){
+
+  let CommentBlockTemplate = `
+  <div class="comment">
+      <div class="comment_header">
+          <img src="${imageSource}" alt="thumb">
+          <p class="name">${name}</p>
+      </div>
+
+      <div class="comment_text">
+          <p>
+            ${text}
+          </p>
+      </div>
+
+      <div class="comment_footer">
+          <p class="date">
+            ${date}
+          </p>
+          <button class="answer">to answer</button>
+      </div>
+
+      <div class="comment_likes">
+          <p>
+              <span class="likes-amount">202</span>
+              <span>
+                  <img src="./img/svg/comment-like.svg" alt="like">
+              </span>
+          </p>
+      </div>
+  </div>
+
+  `
+
+  whereToAddBlk.innerHTML += CommentBlockTemplate;
+
+}
+
+// 
+// Content Editable Placeholder
+// 
+
+// Кастомная textarea для того, что-бы в ней можно было сделать цветной текс
+let ContentEditable = document.querySelectorAll("div[contenteditable='true']");
+
+if(ContentEditable.length > 0){
+  ContentEditable.forEach((el)=>{
+
+    // При фокусе очищаем
+    el.addEventListener("focus", ()=>{
+      if(el.querySelector(".placeholder")){
+        el.innerHTML = "";
+      }
+    })
+
+    // При выходе из фокуса заполняем плейсхолдером
+    el.addEventListener("focusout", ()=>{
+      if(el.innerHTML.length == 0 || el.innerHTML == "<br>"){
+        el.innerHTML = `<span class = "placeholder"> ${el.getAttribute("placeholder")}</span> `
+      }
+    })
+  })
+}
+
+
+
+
+
+
+
+
