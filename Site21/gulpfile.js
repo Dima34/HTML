2dc8e1df683af413057d429350620a75f29f9e32
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-05-18 20:35:01 +0300

    upd

--- Site21/gulpfile.js
+++ Site21/gulpfile.js
@@ -15,7 +15,7 @@ gulp.task("sass-compile", function(){
 })
 
 gulp.task('fileinclude', function() {
-    return gulp.src(['./src/index.html','./src/our-place.html','./src/gallery.html', './src/history.html','./src/contact-us.html'])
+    return gulp.src(['./src/index.html','./src/farmaceutic-museum.html','./src/our-place.html','./src/cruise.html','./src/gallery.html', './src/history.html','./src/contact-us.html'])
       .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
@@ -30,6 +30,7 @@ gulp.task("watch", function(){
     gulp.series("sass-compile"))
     
 })
+
 
 
 
