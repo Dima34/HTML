1feb6c0c6f8fc4f987cf76816a8f7dab5389df78
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-07-23 18:44:01 +0300

    upd

--- Site22_1/js/script.js
+++ Site22_1/js/script.js
@@ -40,7 +40,7 @@ window.onload = function() {
   document.querySelector('.figure-girl').setAttribute('src','./gif/girl_run.gif');
   setTimeout(()=>{
     document.querySelector('.figure-girl').setAttribute('src','./gif/girl_stay.gif');
-  }, 1380)
+  }, 2490)
 };
 
 // document.addEventListener('DOMContentLoaded', ()=>{
