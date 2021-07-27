c2fd9e5b6646ef23c6277f7942c3905138f8fd72
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-07-27 18:31:28 +0300

    upd

--- Site23/gulpfile.js
+++ Site23/gulpfile.js
@@ -15,7 +15,8 @@ gulp.task("sass-compile", function(){
 })
 
 gulp.task('fileinclude', function() {
-    return gulp.src(['./src/index.html'])
+    return gulp.src(['./src/index.html','./src/index-login.html'])
+    
       .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
