7b603b15c848589b84a098a9368cd87a84aa04fd
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-09-17 00:11:16 +0300

    upd

---
+++ Site26/js/script.js
@@ -0,0 +1,7 @@
+console.log('%cDeveloped by Djigit & yur4ik96', 'color: #c434c4; font-size: 50px ');
+
+console.log(
+  '%cTG: @Djigit34 / @yura_k18',
+  'color: #c434c4; background: #eee; font-size: 30px '
+);
+
