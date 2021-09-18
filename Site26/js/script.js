3b7500c36b921a3033d0e21347dd27d117e2c45f
Dima34 <57358582+Dima34@users.noreply.github.com>
2021-09-18 20:16:56 +0300

    mobile added

--- Site26/js/script.js
+++ Site26/js/script.js
@@ -4,3 +4,26 @@ console.log(
   '%cTG: @Djigit34 / @yura_k18',
   'color: #c434c4; background: #eee; font-size: 30px '
 );
+
+const designWidth = 1920;
+const viewportMeta = document.querySelector("meta[name='viewport']");
+const viewportText = "width=device-width, initial-scale=";
+
+function checkWidth(designWidth,viewportMeta,viewportText){
+  if(window.outerWidth > 500){
+    
+    let scale = window.outerWidth / 1920;
+    viewportMeta.setAttribute("content", viewportText+scale);
+
+  } else{
+
+    viewportMeta.setAttribute("content", viewportText+"1");
+
+  }
+}
+
+checkWidth(designWidth,viewportMeta,viewportText);
+
+window.addEventListener("resize", ()=>{
+  checkWidth(designWidth,viewportMeta,viewportText);
+})
